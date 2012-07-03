d41050c42f4e1d4ca4487e983ada43090a301f8d
Shaun <shaunsearle@SOS-iMac.local>
2012-07-03 22:16:40 +0200

    Completed the About Us page, added the products page, removed the help page and added the words only logo to the header

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/products', to: 'static_pages#products'
 
 end
